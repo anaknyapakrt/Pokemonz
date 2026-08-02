b4a75b9cd89207e3735014f657ec6be7b96ab47c
anaknyapakrt <rasyadkrisnamurthi586@gmail.com>
2026-08-02 20:47:59 +0700

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,88 @@
+import aiohttp  # A library for asynchronous HTTP requests
+import random
+
+
+class Pokemon:
+
+    pokemons = {}
+
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+        
+        self.power = random.randint(30,60)
+        self.hp = random.randint(200,400)
+        self.height = 0
+        self.weight = 0
+
+    def update_stats(self, new_power, new_hp):
+        self.power = new_power
+        self.hp = new_hp 
+
+    async def get_name(self):
+        # An asynchronous method to get the name of a pokémon via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+
+                    self.height = data['height']  # Storing the height of the Pokémon
+                    self.weight = data['weight']  # Storing the weight of the Pokémon
+
+                    return data['forms'][0]['name']  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  # Return the default name if the request fails
+
+    async def info(self):
+        # A method that returns information about the pokémon
+        if not self.name:
+            self.name = await self.get_name()  # Retrieving a name if it has not yet been uploaded
+        return f"""Nama Pokemon: {self.name}
+                Tinggi pokemon: {self.height}
+                Berat pokemon: {self.weight}
+                Kekuatan pokemon: {self.power}
+                Kesehatan pokemon: {self.hp}"""
+
+    async def show_img(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API untuk permintaan
+        async with aiohttp.ClientSession() as session:  # Membuka HTTP session
+            async with session.get(url) as response:  # Mengirim permintaan GET
+                if response.status == 200:
+                    data = await response.json()  # Menerima dan mendekode respons JSON
+                    return data['sprites']['front_default']  # Mengembalikan URL gambar Pokémon
+                else:
+                    return None
+
+    async def attack(self, enemy):
+
+        if isinstance(enemy, Wizard):
+            change = random.randint(1, 5)
+            if change==1:
+                return "pokemon penyihir menggunakan perisai selama pertarungan"
+
+
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pertarungan @{self.pokemon_trainer} dengan @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"@{self.pokemon_trainer} menang melawan @{enemy.pokemon_trainer}!"
+        
+class Wizard(Pokemon):
+    pass
+
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        super_power = random.randint(1, 10 )
+        self.power += super_power
+        result = await super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nPokemon petarung menggunakan serangan super. Kekuatan di tambahkan adalah {super_power}!"
---
+++ pokemon.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+from logic import Fighter, Pokemon, Wizard
+import random
+
+# Setting up intents for the bot
+intents = discord.Intents.default()  # Getting the default settings
+intents.messages = True              # Allowing the bot to process messages
+intents.message_content = True       # Allowing the bot to read message content
+intents.guilds = True                # Allowing the bot to work with servers (guilds)
+
+# Creating a bot with a defined command prefix and activated intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# An event that is triggered when the bot is ready to run
+@bot.event
+async def on_ready():
+    # bot.user may be None in some type-checking contexts; use its string representation
+    print(f'Logged in as {bot.user}')  # Outputs the bot's user to the console
+
+# The '!go' command
+@bot.command()
+async def go(ctx):
+    author = ctx.author.name  # Getting the name of the message's author
+    # Check whether the user already has a Pokémon. If not, then...
+    if author not in Pokemon.pokemons.keys():
+        change = random.randint(1, 3)
+        if change == 1:
+            pokemon = Pokemon(author)
+        elif change == 2:
+            pokemon = Wizard(author)
+        else:
+            pokemon = Fighter(author)
+
+        await ctx.send(await pokemon.info())  # Sending information about the Pokémon
+        image_url = await pokemon.show_img()  # Getting the URL of the Pokémon image
+        if image_url:
+            embed = discord.Embed()  # Creating an embed message
+            embed.set_image(url=image_url)  # Setting up the Pokémon's image
+            await ctx.send(embed=embed)  # Sending an embedded message with an image
+        else:
+            await ctx.send("Failed to upload an image of the pokémon.")
+    else:
+        await ctx.send("You've already created your own Pokémon.")  # A message that is printed whether a Pokémon has already been created
+# Running the bot
+
+@bot.command()
+async def attack(ctx):
+    target = ctx.message.mentions[0] if ctx.message.mentions else None
+    if target:
+        if target.name in Pokemon.pokemons and ctx.author.name in Pokemon.pokemons:
+            enemy = Pokemon.pokemons[target.name]
+            attacker = Pokemon.pokemons[ctx.author.name]
+            result = await attacker.attack(enemy)
+            await ctx.send(result)
+        else:
+            await ctx.send("Kedua peserta harus memiliki Pokemon untuk bertarung!")
+    else:
+        await ctx.send("Tetapkan pengguna yang ingin Anda serang dengan menyebut mereka.")
+
+
+bot.run("PUT YOUR TOKEN HERE")  # Running the bot with the token that is provided by Discord for your bot
